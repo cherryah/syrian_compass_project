9427a1f460db0de27b0333ef8dfd3581e8923315
cherryah <ghazalhamodeh444@gmail.com>
2024-08-02 22:41:38 +0300

    dashboard1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Locations from "./Pages/Locations";
 import Resturants from "./Pages/Resturants";
 import Hotels from "./Pages/Hotels";
 import Events from "./Pages/Events";
+import Profile from "./Pages/Profile";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/resturants" Component={Resturants}></Route>
         <Route path="/hotels" Component={Hotels}></Route>
         <Route path="/events" Component={Events}></Route>
+        <Route path="/profile" Component={Profile}></Route>
       </Routes>
     </Router>
   );
