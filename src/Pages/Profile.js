93c930a9fab1e322b73f21745f75a7237ab3cccc
cherryah <ghazalhamodeh444@gmail.com>
2024-08-02 23:00:43 +0300

    profile1

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 import NavBar from '../Component/NavBar'
 import { BsPersonFill } from "react-icons/bs";
 import ReactFlagsSelect from 'react-flags-select';
@@ -7,6 +8,30 @@ import ReactFlagsSelect from 'react-flags-select';
 const Profile = () => {
 
     const [edit, setEdit] = useState(false)
+    const [profile, setProfile] = useState([])
+
+    const ProfileURlAPI='http://127.0.0.1:8000/api/profile'
+    async function Profile(){
+      console.log('ssss')
+         try{
+          const response = await axios.get(ProfileURlAPI, {
+            headers: {
+              "Authorization": `Bearer ${localStorage.getItem('token')}`,
+              "Content-Type": "multipart/form-data",
+              "Access-Control-Allow-Origin": "*"
+            }
+          });
+            console.log(response.data.data)
+            setProfile(response.data.data)
+
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        Profile()
+      }, []);
 
   return (
     <div
@@ -33,26 +58,27 @@ const Profile = () => {
             </div>
         
           <div className='w-[95%] h-[50%] grid grid-cols-3 grid-rows-2 items-center '>
+{/* {profile ? profile.map((item, key) => ( <> */}
             <div className='w-[95%] flex flex-col mb-5 gap-1'>
                         <label>Name</label>
-                        <input type='text' className='outline-none p-2 h-10.5 bg-transparent border-2 border-white rounded-md text-white'/>
+                        <input type='text' value={profile.name} className='outline-none p-2 h-10.5 bg-transparent border-2 border-white rounded-md text-white' disabled/>
                     </div>
                     <div className='w-[95%] flex flex-col mb-5 gap-1'>
                         <label>Email</label>
-                        <input type='email' className='outline-none p-2 h-10.5 bg-transparent border-2 border-white rounded-md text-white'/>
+                        <input type='email' value={profile.email} className='outline-none p-2 h-10.5 bg-transparent border-2 border-white rounded-md text-white' disabled/>
                     </div>
-                    <div className='w-[95%] flex flex-col mb-5 gap-1'>
+                    {/* <div className='w-[95%] flex flex-col mb-5 gap-1'>
                         <label>Password</label>
-                        <input type='password' className='outline-none p-2 h-10.5 bg-transparent border-2 border-white rounded-md text-white'/>
-                    </div>
+                        <input type='password' value={} className='outline-none p-2 h-10.5 bg-transparent border-2 border-white rounded-md text-white'/>
+                    </div> */}
                     <div className='w-[95%] flex flex-col mb-5 gap-1'>
                         <label>Phone Number</label>
-                        <input type='text' className='outline-none p-2 h-10.5 bg-transparent border-2 border-white rounded-md text-white'/>
+                        <input type='text' value={profile.phone_number} className='outline-none p-2 h-10.5 bg-transparent border-2 border-white rounded-md text-white' disabled/>
                     </div>
                     <div className='w-[95%] flex flex-col mb-5 gap-1'>
                         <label>Gender</label>
                         {/* <input type='email' className='outline-none p-2 h-10.5 bg-transparent border-2 border-white rounded-md'/> */}
-                        <select className='outline-none p-2 h-10.5.5 bg-transparent border-2 border-white rounded-md text-white'>
+                        <select value={profile.gender} className='outline-none p-2 h-10.5.5 bg-transparent border-2 border-white rounded-md text-white' disabled>
                             <option></option>
                             <option value='male' className='text-black'>Male</option>
                             <option value='female' className='text-black'>Female</option>
@@ -61,8 +87,9 @@ const Profile = () => {
                     <div className='w-[95%] flex flex-col mb-5 gap-1'>
                         <label>Country</label>
                         {/* <input type='email' className='outline-none p-2 h-10.5 bg-transparent border-2 border-white rounded-md'/> */}
-                        <ReactFlagsSelect className='rounded-md text-black bg-white'/>
+                        <ReactFlagsSelect selected={profile.country}  disabled className='rounded-md text-black bg-white'/>
                     </div>
+                    {/* </>)) : ''} */}
             </div>
             </div>
         </div> 
