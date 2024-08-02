325284b9f0e2cf1b224472615708ea0a6b35462c
cherryah <ghazalhamodeh444@gmail.com>
2024-08-02 22:43:18 +0300

    dashboard2

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -1,8 +1,32 @@
 import React from 'react'
-import { BsSearch } from "react-icons/bs";
+import axios from 'axios';
+import { BsSearch , BsPersonFill, BsArrowBarRight } from "react-icons/bs";
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+
+  const token = localStorage.getItem('token')
+  console.log(token)
+
+  const LogoutURlAPI='http://127.0.0.1:8000/api/logout'
+    async function Logout(){
+      console.log('ssss')
+         try{
+          const response = await axios.post(LogoutURlAPI, null, {
+            headers: {
+              "Authorization": `Bearer ${localStorage.getItem('token')}`,
+              "Content-Type": "multipart/form-data",
+              "Access-Control-Allow-Origin": "*"
+            }
+          });
+            console.log(response)
+            localStorage.removeItem('token')
+            window.location.reload(); 
+        }catch(err){
+            console.log(err)
+        }
+    }
+
   return (
     <div className='w-[100%] h-16 text-[#ffffff] flex justify-evenly items-center bg-[#8c694898] shadow-md shadow-[#8c6948cf] z-50'>
         <div className='w-[20%]'>
@@ -15,10 +39,20 @@ const NavBar = () => {
             <Link to='/events' className='hover:text-[#826950]'>Events</Link>
         </div>
         <div className='w-[25%] flex justify-evenly items-center'>
-            <input type='text' placeholder='search' className='outline-none w-[75%] px-2 py-1.5 rounded-2xl text-white bg-[#ffffff58] placeholder:text-[#ffffff]'/>
+            <input type='text' placeholder='search' className={`outline-none ${token ? 'w-[40%]' : 'w-[75%]'}  px-2 py-1.5 rounded-2xl text-white bg-[#ffffff58] placeholder:text-[#ffffff]`}/>
             <button className='w-10 h-10 bg-[#ffffff58] rounded-full flex justify-center items-center text-[#ffffff]'>
               <BsSearch/>
             </button>
+            {token ? 
+            <>
+            <Link to='/profile' className='w-10 h-10 bg-[#ffffff58] rounded-full flex justify-center items-center text-[#ffffff]'>
+            <BsPersonFill size={23}/>
+          </Link>
+          <button className='w-10 h-10 bg-[#ffffff58] rounded-full flex justify-center items-center text-[#ffffff]' onClick={()=>Logout()}>
+          <BsArrowBarRight size={23}/>
+        </button>
+        </>
+            :''}
         </div>
     </div>
   )
